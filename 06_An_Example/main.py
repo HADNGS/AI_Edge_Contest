6a4061c3c5201f4a703dbc40671c18ef62c884bb
yang-m <yang-m@yang-m>
2019-01-08 17:00:40 +0900

    Revert "Revert "Update:""
    This reverts commit b43d355a002a402fa9022a557de15f219d93a259.

--- 06_An_Example/main.py
+++ 06_An_Example/main.py
@@ -11,11 +11,7 @@ import project_tests as tests
 assert LooseVersion(tf.__version__) >= LooseVersion('1.0'), 'Please use TensorFlow version 1.0 or newer.  You are using {}'.format(tf.__version__)
 print('TensorFlow Version: {}'.format(tf.__version__))
 
-# Check for a GPU
-if not tf.test.gpu_device_name():
-    warnings.warn('No GPU found. Please use a GPU to train your neural network.')
-else:
-    print('Default GPU Device: {}'.format(tf.test.gpu_device_name()))
+
 
 
 def load_vgg(sess, vgg_path):
@@ -144,12 +140,12 @@ tests.test_train_nn(train_nn)
 def run():
     num_classes = 2
     image_shape = (160, 576)  # KITTI dataset uses 160x576 images
-    data_dir = '/data'
-    runs_dir = './runs'
-    tests.test_for_kitti_dataset(data_dir)
+    data_dir = r'E:\Github\AI_Edge_Contest\06_An_Example\vgg'
+    runs_dir = r'E:\Github\AI_Edge_Contest\06_An_Example\runs'
+    #tests.test_for_kitti_dataset(data_dir)
 
     # Download pretrained vgg model
-    helper.maybe_download_pretrained_vgg(data_dir)
+    #helper.maybe_download_pretrained_vgg(data_dir)
 
     # OPTIONAL: Train and Inference on the cityscapes dataset instead of the Kitti dataset.
     # You'll need a GPU with at least 10 teraFLOPS to train on.
@@ -159,10 +155,10 @@ def run():
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
-        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, 'data_road/training'), image_shape)
+        get_batches_fn = helper.gen_batch_function(os.path.join(data_dir, r'data_road\training'), image_shape)
 
-        epochs = 50
-        batch_size = 16
+        epochs = 10
+        batch_size = 32
         correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes])
         learning_rate = tf.placeholder(tf.float32)
 
@@ -187,7 +183,7 @@ def run():
         # save model
         print("Saving Model ...")
         saver = tf.train.Saver()
-        saver.save(sess, './FCN8_Model/fcn8_model')
+        saver.save(sess, r'E:\Github\AI_Edge_Contest\06_An_Example\saveModel')
         print("Model saved")
 
 if __name__ == '__main__':
